4770bff19c75e3481eb0e4c36c9e90e5cadb202f
Richard <richardchekwas@gmail.com>
2023-08-26 11:32:34 +0100

    aa

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# kill
-exec { 'kill':
+# killmenow
+exec { 'killmenow':
   path    => [ '/usr/local/sbin',
                '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
   command => 'pkill killmenow'
+  provider => 'shell',
   }
