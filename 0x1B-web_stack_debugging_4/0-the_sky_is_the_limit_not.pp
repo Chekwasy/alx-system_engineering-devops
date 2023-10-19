83fc22a3dd52cd8e137e3761e4d4bf2bd1f8b854
Chekwasy <richardchekwas@gmail.com>
2023-10-19 21:20:35 +0100

    aa

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,6 @@
-# Increases the amount of traffic an Nginx server can handle.
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin/'
-    } ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-    path    => '/etc/init.d/'
-    }
+# increase number of open fd
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
+}
